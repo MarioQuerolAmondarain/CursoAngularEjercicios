fc38955087046168e4bca3b3a6f8f94edd634259
Mario Querol <marioquerolamondarain@gmail.com>
2022-04-06 15:29:46 +0200

    Funcionamiento implementado falta centrar los centros en sus respectivas circunferencias padres

--- RelojCirculos/main.js
+++ RelojCirculos/main.js
@@ -1,32 +1,43 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-// setInterval(draw(), 10);
-draw();
+
+// draw();
+setInterval(draw, 1000);
 
 function draw()
 {
+    var hoy = new Date()
+    let anguloHoras = hoy.getHours() * Math.PI/6;
+    let anguloMinutos = hoy.getMinutes() * Math.PI/30;
+    let anguloSegundos = hoy.getSeconds() * Math.PI/30;
+    ctx.save();
     ctx.beginPath();
     ctx.arc(200, 200, 190, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.fillStyle = "#B4C7B2";
+    ctx.fill();
+
     
     ctx.beginPath();
-    let x = 0 * Math.PI / 180;
-    ctx.translate(200, 100);
-    ctx.rotate(x);
-    ctx.arc(200, 100, 90, 0, 2 * Math.PI);
+    ctx.translate(200, 200);
+    ctx.rotate(anguloHoras);
+    ctx.arc(0, -100, 90, 0, 2 * Math.PI);
     ctx.fillStyle = "#58C3BB";
     ctx.fill();
-    ctx.rotate(-x);
-    ctx.translate(-200, -100);
-
-    ctx.beginPath();
-    ctx.arc(200, 55, 45, 0, 2 * Math.PI);
-    ctx.fillStyle = "#949A57";
-    ctx.fill();
+    ctx.rotate(-anguloHoras);
     
     ctx.beginPath();
-    ctx.arc(200, 30, 20, 0, 2 * Math.PI);
+    ctx.rotate(anguloMinutos);
+    ctx.arc(0, -145, 45, 0, 2 * Math.PI);
+    ctx.fillStyle = "#949A57";
+    ctx.fill();
+    ctx.rotate(-anguloMinutos);
+    
+    ctx.beginPath();
+    ctx.rotate(anguloSegundos);
+    ctx.arc(0, -165, 20, 0, 2 * Math.PI);
     ctx.fillStyle = "yellow";
     ctx.fill();
+
+    ctx.restore();
 }
