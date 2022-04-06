3c7efc0f7931471bc371f82ee37e6a0bcf864e06
Mario Querol <marioquerolamondarain@gmail.com>
2022-04-06 16:07:13 +0200

    Se termina reloj de circulos

--- RelojCirculos/main.js
+++ RelojCirculos/main.js
@@ -11,12 +11,13 @@ function draw()
     let anguloHoras = hoy.getHours() * Math.PI/6;
     let anguloMinutos = hoy.getMinutes() * Math.PI/30;
     let anguloSegundos = hoy.getSeconds() * Math.PI/30;
+    
     ctx.save();
+    
     ctx.beginPath();
     ctx.arc(200, 200, 190, 0, 2 * Math.PI);
     ctx.fillStyle = "#B4C7B2";
     ctx.fill();
-
     
     ctx.beginPath();
     ctx.translate(200, 200);
@@ -24,20 +25,27 @@ function draw()
     ctx.arc(0, -100, 90, 0, 2 * Math.PI);
     ctx.fillStyle = "#58C3BB";
     ctx.fill();
+    ctx.translate(0, -100);  
     ctx.rotate(-anguloHoras);
     
     ctx.beginPath();
     ctx.rotate(anguloMinutos);
-    ctx.arc(0, -145, 45, 0, 2 * Math.PI);
+    ctx.arc(0, -55, 35, 0, 2 * Math.PI);
     ctx.fillStyle = "#949A57";
     ctx.fill();
+    ctx.translate(0, -55);
     ctx.rotate(-anguloMinutos);
-    
+
     ctx.beginPath();
     ctx.rotate(anguloSegundos);
-    ctx.arc(0, -165, 20, 0, 2 * Math.PI);
+    ctx.arc(0, -25, 10, 0, 2 * Math.PI);
     ctx.fillStyle = "yellow";
     ctx.fill();
 
     ctx.restore();
+}
+function drawContext(ctx)
+{
+    ctx.rect(0, 0, 400, 400);
+    ctx.stroke();
 }
